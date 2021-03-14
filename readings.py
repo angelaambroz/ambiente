b98fc51d237c183e1640666ccc61f3a96b88b568
A little Raspberry Pi <pi@noemail.com>
2021-03-14 19:51:31 -0400

    Beefing up the readings

--- readings.py
+++ readings.py
@@ -11,53 +11,73 @@ ltr = LTR559()
 bme = BME280()
 
 header = ['datetime', 'light', 'proximity', 'temperature', 'humidity']
+data_file = 'data/room.csv'
+
+# Compensating for CPU temp
+# TODO: Fix this
+def get_cpu_temperature():
+    with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+        temp = f.read()
+        temp = int(temp) / 1000.0
+    return temp
+
+def temp_compensate(sensor_temp):
+    # Several temp samples
+    cpu_temps = [get_cpu_temperature()] * 5
+    avg_cpu_temp = sum(cpu_temps) / 5
+    return sensor_temp - avg_cpu_temp
 
 try:
 
-    with open('data/room_mimi.csv', 'w') as f:
+    # Write the header once
+    with open(data_file, 'w') as f:
         writer = csv.writer(f)
         writer.writerow(header)
 
-
-        while True:
-            today = datetime.now().strftime('%Y-%m-%d %H:%S')
-
-            ltr.update_sensor()
-
-            # https://en.wikipedia.org/wiki/Lux
-            # https://www.youtube.com/watch?v=mW6QLkR9ibQ
-            # "how much light hits the surface"
-            lux = ltr.get_lux()
-
-            # https://github.com/pimoroni/ltr559-python/issues/3 
-            # We have no idea what the units are
-            # https://optoelectronics.liteon.com/upload/download/DS86-2013-0003/LTR-559ALS-01_DS_V1.pdf
-            # It's a light sensor - it senses reflected light from objects 
-            # https://en.wikipedia.org/wiki/Proximity_sensor
-            # https://www.youtube.com/watch?v=QfQ_bL8AeGo
-            prox = ltr.get_proximity()
-
-            # https://learn.adafruit.com/adafruit-bme280-humidity-barometric-pressure-temperature-sensor-breakout?view=all
-            # Official docs: https://www.bosch-sensortec.com/media/boschsensortec/downloads/product_flyer/bst-bme280-fl000.pdf
-            # it's celsius
-            temp = bme.get_temperature()
-
-            # https://en.wikipedia.org/wiki/Pascal_(unit)
-            # 1 hectopascal (hPa) = 100 Pa
-            # https://www.sensorsone.com/hpa-hectopascal-pressure-unit/
-            # the international standard for measuring atmospheric/barometric pressure
-            # https://www.youtube.com/watch?v=DquXO2FEl0Q
-            # https://www.youtube.com/watch?v=pTILmF-sx9w low pressure = rain? high pressure = dry?
-            pres = bme.get_pressure()
-
-            # https://en.wikipedia.org/wiki/Humidity
-            # https://learn.adafruit.com/adafruit-bme280-humidity-barometric-pressure-temperature-sensor-breakout?view=all
-            # Relative humidity: "expressed as a percentage, indicates a present state of absolute humidity relative to a maximum humidity given the same temperature" (wiki)
-            hum = bme.get_humidity()
+    while True:
+        f = open(data_file, 'a')
     
-            writer.writerow([today, lux, prox, temp, hum])
+        writer = csv.writer(f)
 
-            time.sleep(2)
+        today = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+        ltr.update_sensor()
+
+        # https://en.wikipedia.org/wiki/Lux
+        # https://www.youtube.com/watch?v=mW6QLkR9ibQ
+        # "how much light hits the surface"
+        lux = ltr.get_lux()
+
+        # https://github.com/pimoroni/ltr559-python/issues/3 
+        # We have no idea what the units are
+        # https://optoelectronics.liteon.com/upload/download/DS86-2013-0003/LTR-559ALS-01_DS_V1.pdf
+        # It's a light sensor - it senses reflected light from objects 
+        # https://en.wikipedia.org/wiki/Proximity_sensor
+        # https://www.youtube.com/watch?v=QfQ_bL8AeGo
+        prox = ltr.get_proximity()
+
+        # https://learn.adafruit.com/adafruit-bme280-humidity-barometric-pressure-temperature-sensor-breakout?view=all
+        # Official docs: https://www.bosch-sensortec.com/media/boschsensortec/downloads/product_flyer/bst-bme280-fl000.pdf
+        # it's celsius
+        temp = bme.get_temperature()
+
+        # https://en.wikipedia.org/wiki/Pascal_(unit)
+        # 1 hectopascal (hPa) = 100 Pa
+        # https://www.sensorsone.com/hpa-hectopascal-pressure-unit/
+        # the international standard for measuring atmospheric/barometric pressure
+        # https://www.youtube.com/watch?v=DquXO2FEl0Q
+        # https://www.youtube.com/watch?v=pTILmF-sx9w low pressure = rain? high pressure = dry?
+        pres = bme.get_pressure()
+
+        # https://en.wikipedia.org/wiki/Humidity
+        # https://learn.adafruit.com/adafruit-bme280-humidity-barometric-pressure-temperature-sensor-breakout?view=all
+        # Relative humidity: "expressed as a percentage, indicates a present state of absolute humidity relative to a maximum humidity given the same temperature" (wiki)
+        hum = bme.get_humidity()
+
+        writer.writerow([today, f"{lux:.2f}", f"{prox:.2f}", f"{temp:.2f}", f"{hum:.2f}"])
+
+        time.sleep(60)
+        f.close()
 
 except KeyboardInterrupt:
     pass
